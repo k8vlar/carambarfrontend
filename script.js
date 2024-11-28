5359cdff968be9264875d63076b63aae23809e89
Kevin Vellard <kevin.vellard@gmail.com>
2024-11-28 20:55:06 +0100

    addfixapiurl

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiUrl = "https://carambar-y6ch.onrender.com/"; // URL dU backend
+const apiUrl = "https://carambar-y6ch.onrender.com/jokes"; 
 
 document.addEventListener('DOMContentLoaded', () => {
     const jokeContainer = document.getElementById('joke-container');
